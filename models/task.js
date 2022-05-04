014060c93829fef23067b4aeebd7f90e170e7cac
pj499 <pjprajakta499@gmail.com>
2022-05-04 21:49:26 +0530

    UI task done

--- models/task.js
+++ models/task.js
@@ -1,1 +1,26 @@
 const mongoose=require('mongoose');
+const taskSchema= new mongoose.Schema({
+    description:{
+        type: String,
+        required: true
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    due_date:{
+        type: String,
+        required: true
+    },
+    due_time:{
+        type: String,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    }
+});
+
+const Task= mongoose.model('Task', taskSchema);
+module.exports= Task;
